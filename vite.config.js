790f501c4de67ff1a77c97dad012d18a2c48b34b
Nextstep-app <tamilexammasters@gmail.com>
2026-05-12 17:30:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures relative paths are used in the build output
+  base: '/', // Ensures relative paths are used in the build output
 })
